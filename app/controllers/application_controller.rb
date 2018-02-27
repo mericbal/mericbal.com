5f383c3dd8053b961910b34222f020e974c43cf8
Meric BAL <MB@Meric-MacBook-Air.local>
2018-02-27 15:49:13 -0800

    Some modifications.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+  	# @comments = Comment.all
+  	@comment = Comment.new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'mains#index'
+  root to: 'application#index'
 
-  get '/' => 'mains#index'
-  get '/portfolio' => 'mains#portfolio'
+  # get '/' => 'mains#index'
+  get '/portfolio' => 'application#portfolio'
 
   resource :comments, only: [:new, :create]
 end
