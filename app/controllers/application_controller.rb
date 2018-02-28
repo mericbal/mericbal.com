4a15f95e8ff0dea995dde02c2a504dca750bc5ee
Meric BAL <MB@Meric-MacBook-Air.local>
2018-02-27 19:39:02 -0800

    Update.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
+  	sleep 0.5
   	# @comments = Comment.all
   	@comment = Comment.new
   end
